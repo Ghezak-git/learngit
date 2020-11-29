026e8be29c7ea93ce1c8878139cc91f8d291da08
Dede F <thithit630@gamil.com>
2020-11-29 18:58:06 +0700

    push admin controller

--- application/controllers/administrator.php
+++ application/controllers/administrator.php
@@ -8,7 +8,7 @@ class Administrator extends CI_Controller {
         $this->load->helper('cookie');
         $this->load->library('form_validation');
         // $this->load->model('Categories_model');
-        // $this->load->model('Products_model');
+        $this->load->model('Products_model');
         // $this->load->model('Settings_model');
         // $this->load->model('Promo_model');
         // $this->load->model('Testi_model');
@@ -46,5 +46,37 @@ class Administrator extends CI_Controller {
         session_destroy();
         delete_cookie('djehbicd');
         redirect(base_url() . 'login/admin');
+    }
+
+    public function products(){
+        $data['title'] = 'Product - Admin Panel';
+        $config['base_url'] = base_url() . 'administrator/products/';
+        // $config['total_rows'] = $this->Products_model->getProducts("","")->num_rows();
+        // $config['per_page'] = 10;
+        // $config['first_link']       = 'First';
+        // $config['last_link']        = 'Last';
+        // $config['next_link']        = 'Next';
+        // $config['prev_link']        = 'Prev';
+        // $config['full_tag_open']    = '<div class="pagging text-center"><nav><ul class="pagination justify-content-center">';
+        // $config['full_tag_close']   = '</ul></nav></div>';
+        // $config['num_tag_open']     = '<li class="page-item"><span class="page-link">';
+        // $config['num_tag_close']    = '</span></li>';
+        // $config['cur_tag_open']     = '<li class="page-item active"><span class="page-link">';
+        // $config['cur_tag_close']    = '<span class="sr-only">(current)</span></span></li>';
+        // $config['next_tag_open']    = '<li class="page-item"><span class="page-link">';
+        // $config['next_tagl_close']  = '<span aria-hidden="true">&raquo;</span></span></li>';
+        // $config['prev_tag_open']    = '<li class="page-item"><span class="page-link">';
+        // $config['prev_tagl_close']  = '</span>Next</li>';
+        // $config['first_tag_open']   = '<li class="page-item"><span class="page-link">';
+        // $config['first_tagl_close'] = '</span></li>';
+        // $config['last_tag_open']    = '<li class="page-item"><span class="page-link">';
+        // $config['last_tagl_close']  = '</span></li>';
+        // $from = $this->uri->segment(3);
+        // $this->pagination->initialize($config);
+        // $data['getProducts'] = $this->Products_model->getProducts($config['per_page'], $from);
+        $data['getProducts'] = $this->Products_model->getProducts();
+        $this->load->view('templates/header_admin', $data);
+        $this->load->view('administrator/products', $data);
+        $this->load->view('templates/footer_admin');
     }
 }
